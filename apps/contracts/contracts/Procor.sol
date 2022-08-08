c9ec10e1b76a9d812b957b8aed91e52737b48272
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-08-08 19:45:13 -0400

    created general froamt for contract

---
+++ apps/contracts/contracts/Procor.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@semaphore-protocol/contracts/interfaces/IVerifier.sol";
+import "@semaphore-protocol/contracts/base/SemaphoreCore.sol";
+import "@semaphore-protocol/contracts/base/SemaphoreGroups.sol";
+
+contract Procor {
+    
+    // events
+
+    // new sesssion added
+    // sesssion started
+    // sesssion ended
+
+    // user joined
+    // user left
+
+    // question asekd
+
+    // state
+
+    // mappings
+
+    // session owner => session struct
+    // session owner => identity commitments
+
+    // verifier 
+
+    // structs
+
+    // session struct
+    // question struct
+
+    // modifiers
+
+    // {don't know yet}
+
+    // functions
+
+    // cosntructor
+    // make session
+    // start session
+    // end session
+    // join session
+    // ask question
+    // vote for question
+
+
+
+    
+}
